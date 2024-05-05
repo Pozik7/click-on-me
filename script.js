bdd59d78835931f5147632bebe8722f09b23163e
Pozik7 <leoleo4821@gmail.com>
2024-05-06 02:15:52 +0300

    CLick on me button added

---
+++ script.js
@@ -0,0 +1,6 @@
+
+var button = document.getElementById("myButton");
+
+button.addEventListener("click", function() {
+  alert("I have completed the task as per your instructions. May we now proceed?");
+});
