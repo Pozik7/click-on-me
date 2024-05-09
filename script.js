ed1a86c6e8d546f73310434b8931fa88ac5a734c
Pozik7 <leoleo4821@gmail.com>
2024-05-09 16:35:06 +0300

    html change

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-
 var button = document.getElementById("myButton");
 
 button.addEventListener("click", function() {
-  alert("I have completed the task as per your instructions. May we now proceed?");
+  var messageContainer = document.createElement("div");
+  messageContainer.textContent = "I have completed the task as per your instructions. May we now proceed?";
+  messageContainer.classList.add("message");
+  var container = document.querySelector(".container");
+  container.appendChild(messageContainer);
 });
